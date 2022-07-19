3e970e98e77443aa9d4e86ea53d73811f9e1511b
Carlos <carlosvaldivia390@gmail.com>
2022-07-19 17:47:38 -0400

    Changed from class to function the App component

--- src/App.js
+++ src/App.js
@@ -1,98 +1,69 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import Preview from "./components/Preview";
 import uniqid from "uniqid";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      displayForm: true,
-      personalInfo: {
-        fullName: 'Full Name',
-        profession: 'Profession',
-        phoneNumber: 'Phone Number',
-        email: 'E-Mail',
-        address: 'Address',
-        description: 'Description',
-      },
-      jobs: [],
-      education: [],
-      submitted: false,
-      previewButton: 'Edit',
-    }
-    this.onSubmit = this.onSubmit.bind(this);
-    this.handlePersonalChange = this.handlePersonalChange.bind(this);
-    this.handleGroupChange = this.handleGroupChange.bind(this);
-    this.addJob = this.addJob.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.addEducation = this.addEducation.bind(this);
-    this.onEdit = this.onEdit.bind(this);
+const App = () => {
+  const [personalInfo, setPersonalInfo] = useState({
+    fullName: 'Full Name',
+    profession: 'Profession',
+    phoneNumber: 'Phone Number',
+    email: 'E-Mail',
+    address: 'Address',
+    description: 'Description',
+  })
+  const [groups, setGroups] = useState({jobs: [], education: []});
+  const [submitted, setSubmitted] = useState(false);
+  const [previewButton, setPreviewButton] = useState('Edit');
+  const [displayForm, setDisplayForm] = useState(true);
+
+  const handlePersonalChange = (group, name, id, e) => {
+    setPersonalInfo(state => ({ ...state, [name]: e.target.value}));
   }
 
-  handlePersonalChange = (group, name, id, e) => {
-    this.setState(() => {
-      this.state['personalInfo'][name] = e.target.value;      
-    });
-    console.log(this.state.personalInfo)
-  }
-
-  handleGroupChange = (group, name, id, e) => {
+  const handleGroupChange = (group, name, id, e) => {
     let index;
-    this.state[group].forEach(item => {
+    groups[group].forEach(item => {
       if(item.id === id)
-         index = this.state[group].indexOf(item);
+         index = groups[group].indexOf(item);
     })
-    this.setState(() => {
-      this.state[group][index][name] = e.target.value;
-      })
+    let aux = groups[group];
+    aux[index][name] = e.target.value;
+    setGroups(state => ({...state, aux}))
   }
 
-  handleDelete = (group, id, e) => {
-    this.setState({
-      [group]: this.state[group].filter(item => {
-        if (item.id !== id) return item;
-      })
-    })
+  const handleDelete = (group, id, e) => {
+    setGroups(state => ({ ...state, [group]: groups[group].filter(item => {
+      if (item.id !== id) return item;
+    })}))
   }
 
-  addJob = (name, e) => { 
-    this.setState({
-      jobs: this.state.jobs.concat({
-        position: 'Position',
+  const addJob = (name, e) => { 
+    setGroups(state => ({ ...state, jobs: groups.jobs.concat({
+      position: 'Position',
         company: 'Company',
         city: 'City',
         start: 'From(YYYY)',
         end: 'To(YYYY)',
         description: 'Description',
         id: uniqid(),
-      })
-    })
+    })}))
   }
 
-  addEducation = (name, e) => { 
-    this.setState({
-      education: this.state.education.concat({
-        course: 'Carrer/Course',
+  const addEducation = (name, e) => { 
+    setGroups(state => ({ ...state, education: groups.education.concat({
+      course: 'Carrer/Course',
         institution: 'Institution',
         start: 'From(YYYY)',
         end: 'To(YYYY)',
         id: uniqid(),
-      })
-    })
+    })}))
   }
 
-  onSubmit = () => {
-    this.setState({
-      displayForm: false,
-    })
-  }
-
-  onEdit = () => {
-    console.log(this.state.submitted)
+  const onEdit = () => {
     let bool, 
         text;
-    if (this.state.submitted === false) {
+    if (submitted === false) {
       bool = true;
       text = 'Submit';
     }
@@ -100,40 +71,40 @@ class App extends Component {
       bool = false;
       text = 'Edit';
     }
-    this.setState({
-      previewButton: text,
-      submitted: bool,
-    })
-  } 
-
-  render() {
-    const { displayForm, jobs, education, personalInfo, submitted, previewButton } = this.state;
-    if(displayForm === true)
-    return ( 
-      <div>
-        <h1>CV Application</h1>
-        <Form edit = {displayForm} handleChange = {this.handlePersonalChange} jobs = {jobs}
-            handleGroupChange = {this.handleGroupChange} addJob = {this.addJob}
-            handleDelete = {this.handleDelete} addEducation = {this.addEducation}
-            education = {education} onSubmit = {this.onSubmit} personalInfo = {personalInfo}/> 
-        <Preview edit = {submitted} handleChange = {this.handlePersonalChange} jobs = {jobs}
-            handleGroupChange = {this.handleGroupChange} addJob = {this.addJob}
-            handleDelete = {this.handleDelete} addEducation = {this.addEducation}
-            education = {education} onEdit = {this.onEdit} previewButton = {previewButton}
-            personalInfo = {personalInfo} hide = {true}/>     
-      </div>
-    )
-    else
-    return (
-      <div>
-      <h1>CV Application</h1>
-        <Preview edit = {submitted} handleChange = {this.handlePersonalChange} jobs = {jobs}
-            handleGroupChange = {this.handleGroupChange} addJob = {this.addJob}
-            handleDelete = {this.handleDelete} addEducation = {this.addEducation}
-            education = {education} onEdit = {this.onEdit} previewButton = {previewButton}
-            personalInfo = {personalInfo} hide = {false}/>  
-      </div>
-    )
+    setSubmitted(bool);
+    setPreviewButton(text);
   }
+
+  const onSubmit = () => {
+    setDisplayForm(false);
+  }
+
+  if(displayForm === true)
+  return ( 
+    <div>
+      <h1>CV Application</h1>
+      <Form edit = {displayForm} handleChange = {handlePersonalChange} jobs = {groups.jobs}
+          handleGroupChange = {handleGroupChange} addJob = {addJob}
+          handleDelete = {handleDelete} addEducation = {addEducation}
+          education = {groups.education} onSubmit = {onSubmit} personalInfo = {personalInfo}/> 
+      <Preview edit = {submitted} handleChange = {handlePersonalChange} jobs = {groups.jobs}
+          handleGroupChange = {handleGroupChange} addJob = {addJob}
+          handleDelete = {handleDelete} addEducation = {addEducation}
+          education = {groups.education} onEdit = {onEdit} previewButton = {previewButton}
+          personalInfo = {personalInfo} hide = {true}/>     
+    </div>
+  )
+  else
+  return (
+    <div>
+    <h1>CV Application</h1>
+      <Preview edit = {submitted} handleChange = {handlePersonalChange} jobs = {groups.jobs}
+          handleGroupChange = {handleGroupChange} addJob = {addJob}
+          handleDelete = {handleDelete} addEducation = {addEducation}
+          education = {groups.education} onEdit = {onEdit} previewButton = {previewButton}
+          personalInfo = {personalInfo} hide = {false}/>  
+    </div>
+  ) 
 }
+
 export default App;
